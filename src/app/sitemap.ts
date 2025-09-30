d9fd434dd0cfb250b072a44e27892c8dbfaaead9
gsjoy24 <gour.joy24@gmail.com>
2025-09-30 20:52:17 +0600

    cc

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: 'https://goursahajoy.me',
+			url: 'https://joy.vertoone.com',
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
 			priority: 1
 		},
 		{
-			url: 'https://goursahajoy.me/blogs',
+			url: 'https://joy.vertoone.com/blogs',
 			lastModified: new Date(),
 			changeFrequency: 'weekly',
 			priority: 0.8
 		},
 		{
-			url: 'https://goursahajoy.me/contact',
+			url: 'https://joy.vertoone.com/contact',
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
 			priority: 0.5
