2880d83521747975689d50dac9d2836ba21a1c27
gsjoy24 <gour.joy24@gmail.com>
2025-09-30 21:02:03 +0600

    cc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import type { Metadata } from "next";
 import { Toaster } from "react-hot-toast";
 import { dm_sans } from "./fonts";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
 	title: "Gour Saha Joy - Full-Stack Web Developer",
@@ -33,7 +32,6 @@ export default function RootLayout({ children }: { readonly children: React.Reac
 		<Providers>
 			<html lang='en'>
 				<GoogleAnalytics />
-				<Analytics />
 				<body className={`${dm_sans.className}`}>
 					<AppRouterCacheProvider>
 						{children}
